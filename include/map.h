5b0422356aac2c48df4395aa9e94e75ec4b4cdad
junhpark <feldblume5263@gmail.com>
2020-11-20 15:22:55 +0900

    update code

---
+++ include/map.h
@@ -0,0 +1,27 @@
+#ifndef MAP_H
+#define MAP_H
+
+# include <iostream>
+# include <unistd.h>
+# include <stdio.h>
+
+class			map
+{
+private:
+	int *map_file;
+public:
+	map();
+	~map();
+};
+
+map::map()
+{
+}
+
+map::~map()
+{
+}
+
+
+
+#endif
---
+++ src/map.cpp
@@ -0,0 +1,2 @@
+#include "../include/map.h"
+
