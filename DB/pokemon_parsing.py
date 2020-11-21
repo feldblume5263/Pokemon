fc83fdb31f9789a7f175b207c64e13835cfa93e1
junbangg <wnstjr4620@naver.com>
2020-11-21 23:19:43 +0900

    Parsing Stats

--- DB/pokemon_parsing.py
+++ DB/pokemon_parsing.py
@@ -1,10 +1,16 @@
 import requests
 import json
-
 '''
+searchPokemon()
+
+@Parameters:
+    -pokemon name (string)
+=>
     name : 포켓몬 이름(str)
     moves : 포켓몬 기술(list)
-
+    movesLength : 기술 수
+    power : 힘? (int)
+    pp: ?? (int)
 '''
 def searchPokemon(name):
     try:
@@ -15,9 +21,14 @@ def searchPokemon(name):
     else:
         name = pokemon['name']
         moves = list(i['move'] for i in pokemon['moves'])
-        #출력
-        print(json.dumps(moves, indent = 4, sort_keys = True))
-
+        movesLength = len(moves)
+        # 기술 수치 찾기
+        move_response = requests.get(moves[0]['url'])
+        move_data = json.loads(move_response.text)
+        power = move_data['power']
+        pp = move_data['pp']
+        #json 예쁘게 출력
+        #print(json.dumps(move_data, indent = 4, sort_keys = True))
 
 
 searchPokemon("pikachu")
