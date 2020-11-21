25886d96cf9166c44f66cdc526aba35e715ea576
junhpark <feldblume5263@gmail.com>
2020-11-21 18:06:05 +0900

    update code

--- include/map.h
+++ include/map.h
@@ -4,14 +4,28 @@
 # include <iostream>
 # include <unistd.h>
 # include <stdio.h>
+# include <vector>
+#include <termios.h>
+#include <unistd.h>
+
+# define M_KEY_UP		119
+# define M_KEY_LEFT		97
+# define M_KEY_RIGHT	100
+# define M_KEY_DOWN		115
+# define M_KEY_SPACE	32
+
 
 class			map
 {
 private:
-	int *map_file;
+
 public:
 	map();
 	~map();
+
+	int pjh_getch();
+	void handle_key(int key);
+
 };
 
 map::map()
@@ -21,7 +35,5 @@ map::map()
 map::~map()
 {
 }
-
-
 
 #endif
--- src/map.cpp
+++ src/map.cpp
@@ -1,2 +1,50 @@
 #include "../include/map.h"
 
+void		map::handle_key(int key)
+{
+	if (key == M_KEY_UP)
+	{
+		std::cout << "up" << std::endl;
+	}
+	else if (key == M_KEY_DOWN)
+	{
+		std::cout << "down" << std::endl;
+	}
+	else if (key == M_KEY_RIGHT)
+	{
+		std::cout << "right" << std::endl;
+	}
+	else if (key == M_KEY_LEFT)
+	{
+		std::cout << "left" << std::endl;
+	}
+	else
+		return ;
+}
+
+int			map::pjh_getch()
+{
+	struct	termios oldt;
+	struct	termios newt;
+	int		ch;
+
+	tcgetattr( STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~( ICANON | ECHO);
+	tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+	ch = getchar();
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+	return (ch);
+}
+
+int				main(void)
+{
+	map			map;
+	int			pressed;
+
+	while (1)
+	{
+		pressed = map.pjh_getch();
+		map.handle_key(pressed);
+	}
+}
