a96f2496f22cd3d99adcfeaaa368a4b89f931613
JUNseo50000 <junseo50000@gmail.com>
2020-11-24 00:22:47 +0900

    embody basic algorithms

--- battle/Battle.cpp
+++ battle/Battle.cpp
@@ -1,22 +1,19 @@
 ﻿#include "Battle.h"
 
-Battle::Battle(Player* _my_player, Player* _other_player)
+Battle::Battle(MyPlayer* _my_player, OtherPlayer* _other_player)
+    :mersenne(static_cast<unsigned int>(std::time(nullptr)))
 {
+
     my_player = _my_player;
     other_player = _other_player;
 
-
-    //std::cout << "my_player : " << my_player << std::endl;
-    //std::cout << "_my_player : " << _my_player << std::endl;
-    std::cout << "my_player->pokemons[0] : " << my_player->pokemons[0]->Attack << std::endl;
-    std::cout << "_my_player->pokemons[0] : " << _my_player->pokemons[0]->Attack << std::endl;
-
-    pullPokemon();
+    startBattle();
 }
 
-Battle::Battle(Player* _my_player, Pokemon* _other_pokemon)
+Battle::Battle(MyPlayer* _my_player, Pokemon* _other_pokemon)
 {
    // todo
+    startHunting();
 }
 
 void Battle::startBattle()
@@ -25,31 +22,73 @@ void Battle::startBattle()
 
     while (endBattle == false)
     {
-        // pokemon can not fight or change pokemon
-        while (my_selected_pokemon->liveState == true || other_selected_pokemon->liveState == true
-            || changeState == false)
-        {
-            selectAction();
-        }
-
-        // todo : check all pokemon can not fight
-        changePokemon();
+        selectAction();
     }
 
+    // end battle
+    if (my_win == true)
+    {
+        display();
+        gotoxy(5, 15);
+        std::cout << "My Win" << std::endl;
+    }
+    else
+    {
+        display();
+        gotoxy(5, 15);
+        std::cout << "My Loose" << std::endl;
+    }
+}
 
+inline void Battle::startHunting()
+{
 }
 
 void Battle::display()
 {
+    system("cls");
+
     gotoxy(5, 12);
     std::cout << my_selected_pokemon->name << std::endl;
     gotoxy(5, 12 + 1);
-    std::cout << "HP : " << my_selected_pokemon->HP << std::endl;
+    printHPbar(my_selected_pokemon);
+    gotoxy(5, 12 + 1 + 1);
+    std::cout << "HP : " << my_selected_pokemon->HP << " / " << my_selected_pokemon->maxHP << std::endl;
 
     gotoxy(40, 2);
     std::cout << other_selected_pokemon->name << std::endl;
     gotoxy(40, 2 + 1);
-    std::cout << "HP : " << other_selected_pokemon->HP << std::endl;
+    printHPbar(other_selected_pokemon);
+    gotoxy(40, 2 + 1 + 1);
+    std::cout << "HP : " << other_selected_pokemon->HP << " / " << other_selected_pokemon->maxHP << std::endl;
+}
+
+void Battle::printHPbar(Pokemon* pokemon)
+{
+    int num = 0;
+    int checkHP = 0;
+    int standardHP = pokemon->maxHP / 10;
+
+    // max num is 10
+    while (num < 10)
+    {
+        if (pokemon->HP > checkHP)
+        {
+            num += 1;
+            checkHP += standardHP;
+        }
+        else
+            break;
+    }
+    
+
+    for (int i = 0; i < num; ++i)
+    {
+        std::cout << "O";
+        //std::cout << i;
+    }
+    std::cout << std::endl;
+
 }
 
 void Battle::pullPokemon()
@@ -59,61 +98,382 @@ void Battle::pullPokemon()
     {
         my_selected_pokemon = my_player->pokemons[0];
         other_selected_pokemon = other_player->pokemons[0];
-
-        //std::cout << my_player->pokemons[0]->HP << std::endl;
-
-        //std::cout << my_player->pokemons[0]->Attack << std::endl;
-        //std::cout << "TEST" << std::endl;
-        //std::cout << my_player->pokemons.size() << std::endl;
-        //std::cout << (my_player->pokemons[0]->Attack) << std::endl;
     }
-
-    //checkSpeed();
+    checkSpeed();
 }
-
-
 
 void Battle::selectAction()
 {
     display();
 
-    int x = 5; int y = 15;
+    int x = 7; int y = 15;
 
     gotoxy(x, y);
-    std::cout << "__ FIGHT" << std::endl;
-    std::cout << "     __ BAG" << std::endl;
-    std::cout << "     __ POKEMON" << std::endl;
-    std::cout << "     __ RUN" << std::endl;
+    std::cout << "FIGHT" << std::endl;
+    gotoxy(x, ++y);
+    std::cout << "BAG" << std::endl;
+    gotoxy(x, ++y);
+    std::cout << "POKEMON" << std::endl;
+    gotoxy(x, ++y);
+    std::cout << "RUN" << std::endl;
 
-    gotoxy(x, y);
     // todo : delete Overlapping code
-    std::cout << "O\b";
-    while (1)
+    y -= 3;
+    gotoxy(x - 2, y);
+    std::cout << ">\b";
+    while (getArrowkey(x, y) == false)
     {
-        if (getArrowkey(x, y) == true)
-            break;
-        gotoxy(x, y);
-        std::cout << "O\b";
+        gotoxy(x - 2, y);
+        std::cout << ">\b";
     }
 
-    // FIGHT
+    // select
     if (y == 15)
         selectMove();
     else if (y == 16)
-        selectPokemon();
+        selectBag();
+    else if (y == 17)
+        changePokemon(my_player);
+    else if (y == 18)
+        selectRun();
 }
 
+// Attack
+void Battle::selectMove()
+{
+    display();
+
+    int x = 5, y = 14;
+    gotoxy(x, y);
+    std::cout << "     __MOVES__" << std::endl;
+    for (int i = 0; i < 4; ++i)
+        std::cout << "        | TYPE : " << my_selected_pokemon->move[i].type << " | PP : " << 
+            my_selected_pokemon->move[i].pp << " |" << my_selected_pokemon->move[i].name << std::endl;;
+
+    gotoxy(x, ++y);
+    std::cout << ">\b";
+    while (getArrowkey(x, y) == false)
+    {
+        gotoxy(x, y);
+        std::cout << ">\b";
+    }
+
+    if (y == 15)
+        fight(my_selected_pokemon, other_selected_pokemon, my_selected_pokemon->move[0]);
+    else if (y == 16)
+        fight(my_selected_pokemon, other_selected_pokemon, my_selected_pokemon->move[1]);
+    else if (y == 17)
+        fight(my_selected_pokemon, other_selected_pokemon, my_selected_pokemon->move[2]);
+    else if (y == 18)
+        fight(my_selected_pokemon, other_selected_pokemon, my_selected_pokemon->move[3]);
+}
+
+void Battle::fight(Pokemon* myPokemon, Pokemon* otherPokemon, Move& move)
+{
+    display();
+
+    // my pokemon will bi atack at first.
+    if (priority == true)
+    {
+        attack(myPokemon, otherPokemon, move);
+        // other pokemon can not fight
+        if (otherPokemon->liveState == false)
+        {
+            std::cout << "otherPokemon->liveState == false" << std::endl;
+            // other player has another pokemon
+            if (checkPokemons(other_player) == true)
+            {
+                std::cout << "checkPokemons(other_player) == true" << std::endl;
+
+                while(!getEnterSpacebar());
+                
+                changePokemon(other_player);
+                return;
+            }
+            // end battle
+            else {
+                my_win = true; return;
+            }
+        }
+
+        // setMove by random.
+        int ix = setRandomMoveNumber();
+        attack(otherPokemon, myPokemon, other_selected_pokemon->move[ix]);
+        if (myPokemon->liveState == false)
+        {
+            if (checkPokemons(my_player) == true)
+            {
+                changePokemon(my_player);
+                return;
+            }
+            else {
+                my_win = false; return;
+            }
+        }
+    }
+    // other pokemon will bi atack at first.
+    else if (priority == false)
+    {
+        int ix = setRandomMoveNumber();
+        attack(otherPokemon, myPokemon, other_selected_pokemon->move[ix]);
+        if (myPokemon->liveState == false)
+        {
+            if (checkPokemons(my_player) == true)
+            {
+                changePokemon(my_player);
+                return;
+            }
+            else {
+                my_win = false; return;
+            }
+        }
+
+
+        attack(myPokemon, otherPokemon, move);
+        if (otherPokemon->liveState == false)
+        {
+            if (checkPokemons(other_player) == true)
+            {
+                changePokemon(other_player);
+                return;
+            }
+            else {
+                my_win = true; return;
+            }
+        }
+    }
+}
+
+void Battle::attack(Pokemon* attakingPokemon, Pokemon* defendingPokemon, Move& move)
+{
+    display();
+    int x = 5, y = 15;
+    gotoxy(x, y);
+    int damage = calculateDamage(attakingPokemon, defendingPokemon, move) * 0.1f;
+    std::cout << attakingPokemon->name << " Used " << move.name << "." << std::endl;
+    while (!getEnterSpacebar());
+
+    display();
+    gotoxy(x, y);
+    std::cout << attakingPokemon->name << " Damaged " << damage << " " << defendingPokemon->name << std::endl;
+    while (!getEnterSpacebar());
+
+
+    // todo : hp bar will be diminished slowly
+    //for (int i = 0; i < damage; ++i)
+    //{
+    //    defendingPokemon->HP = defendingPokemon->HP - 1;
+    //    Sleep(100);
+    //}
+
+    defendingPokemon->HP = defendingPokemon->HP - damage;
+    move.pp -= 1;
+    checkAlive(defendingPokemon);
+
+}
+
+int Battle::calculateDamage(const Pokemon* const attakingPokemon, const Pokemon* const defendingPokemon, const Move& move)
+{
+    float SametypeAttackbonus = checkSametypeAttackbonus(attakingPokemon, move);
+ 
+    float Decision = 0.0f;
+    // normal Attack
+    if (move.move_type == 0)
+        Decision = attakingPokemon->Attack * move.power * SametypeAttackbonus;
+    // sepcial Attack
+    else if (move.move_type == 1)
+        Decision = attakingPokemon->SpecialAttack * move.power * SametypeAttackbonus;
+
+    // 0.411 is correction dimension
+    float durability = 0.0f;
+    durability = defendingPokemon->HP * defendingPokemon->Defence * 0.411;
+
+    // Damage calculate
+
+    float HitSpot = checkHitSpot();
+    float Type1 = calculateType();
+    float Type2 = calculateType();
+
+    float damage = 0.0f;
+    int RandomNumber = getRandomNumber();
+    damage = Decision / durability * HitSpot * Type1 * Type2 * RandomNumber;
+
+    return static_cast<int>(damage);
+}
+
+float Battle::checkSametypeAttackbonus(const Pokemon* const attakingPokemon, const Move& move)
+{
+    // todo
+    return 1.0f;
+}
+
+float Battle::calculateType()
+{
+    // todo
+    return 1.0f;
+}
+
+float Battle::checkHitSpot()
+{
+    // 6.25%
+    int RN = 0;
+    RN = std::uniform_int_distribution<>(1, 16)(mersenne);
+    if (RN == 16)
+        return 2.0f;
+    else
+        return 1.0f;
+}
+
+int Battle::getRandomNumber()
+{
+    float RN = 0.0f;
+    RN = std::uniform_int_distribution<>(217, 255)(mersenne) * 100.0f / 255.0f;
+    return static_cast<int>(RN);
+}
+
+void Battle::checkAlive(Pokemon* pokemon)
+{
+    if (pokemon->HP <= 0)
+    {
+        pokemon->HP = 0;
+        pokemon->liveState = false;
+    }
+}
+
+bool Battle::checkPokemons(const Player* const player)
+{
+    endBattle = true;
+
+    for (auto pokemon : player->pokemons)
+    {
+        // at least one of pokemons is alive
+        if (pokemon->liveState == true)
+        {
+            endBattle = false;
+            return true;
+        }
+    }
+    // battle end
+    return false;
+}
+
+// todo : more efficient
+int Battle::setRandomMoveNumber()
+{
+    for (int i = 0; i < 4; ++i)
+    {
+        // at least one move have a pp
+        if (other_selected_pokemon->move[i].pp > 0)
+        {
+            while (1)
+            {
+                int ix = std::uniform_int_distribution<>(0, 3)(mersenne);
+                if (other_selected_pokemon->move[ix].pp > 0)
+                    return ix;
+            }
+        }
+    }
+
+    // Can not use move sice don`t have pp
+    // todo
+    return 5;
+}
 
 // change
-void Battle::selectPokemon()
+int Battle::selectPokemon()
 {
-    changeState = true;
+    // todo : show pokemon`s emoji
+    display();
+    for (int i = 0; i < my_player->pokemons.size(); ++i)
+    {
+        gotoxy(5, 17 + 3 * i);
+        std::cout << "  " << my_player->pokemons[i]->name << std::endl;
+        gotoxy(5, 17 + 3 * i + 1);
+        std::cout << "  HP : " << my_player->pokemons[i]->HP << " / " << my_player->pokemons[i]->maxHP << std::endl;
+    }
+
+    // todo : delete Overlapping code
+    gotoxy(5, 17);
+    int x = 5, y = 17;
+    std::cout << ">\b";
+    while (1)
+    {
+        // todo : set interval
+        gotoxy(x , y);
+        std::cout << ">\b";
+        if (getArrowkey(x, y) == true)
+        {
+            // todo : HP > 0
+            if (y == 17)
+            {
+                display();
+                gotoxy(5, 15);
+                std::cout << "Can`t swap" << std::endl;
+                while (!getEnterSpacebar());
+                display();
+
+                for (int i = 0; i < my_player->pokemons.size(); ++i)
+                {
+                    gotoxy(5, 17 + 3 * i);
+                    std::cout << "  " << my_player->pokemons[i]->name << std::endl;
+                    gotoxy(5, 17 + 3 * i + 1);
+                    std::cout << "  HP : " << my_player->pokemons[i]->HP << " / " << my_player->pokemons[i]->maxHP << std::endl;
+                }
+
+                continue;
+            }
+            else
+                // todo
+                return 1;
+
+        }
+    }
 }
 
-void Battle::changePokemon()
+void Battle::changePokemon(MyPlayer* player)
 {
-    ;
-    // change pokemon
+    int ix = selectPokemon();
+
+    display();
+    gotoxy(5, 15);
+    std::swap(player->pokemons[0], player->pokemons[ix]);
+    std::cout << "IN " << player->pokemons[ix]->name << std::endl;
+    while (!getEnterSpacebar());
+
+    gotoxy(5, 15);
+    std::cout << "OUT " << player->pokemons[0]->name << std::endl;
+    while (!getEnterSpacebar());
+
+    // change current pokemon
+    my_selected_pokemon = player->pokemons[0];
+    display();
+    while (!getEnterSpacebar());
+
+}
+void Battle::changePokemon(OtherPlayer* player)
+{
+    int ix = 0;
+    while (1)
+    {
+        ix = std::uniform_int_distribution<>(1, player->pokemons.size() - 1)(mersenne);
+        if (player->pokemons[ix]->HP > 0)
+            break;
+    }
+
+    display();
+    gotoxy(5, 15);
+    std::swap(player->pokemons[0], player->pokemons[ix]);
+    std::cout << "IN " << player->pokemons[ix]->name << std::endl;
+    while (!getEnterSpacebar());
+
+    gotoxy(5, 15);
+    std::cout << "OUT " << player->pokemons[0]->name << std::endl;
+    while (!getEnterSpacebar());
+
+    // change current pokemon
+    other_selected_pokemon = player->pokemons[0];
+    display();
+    while (!getEnterSpacebar());
 }
 
 void Battle::checkSpeed()
@@ -124,9 +484,87 @@ void Battle::checkSpeed()
         priority = false;
     else if (my_selected_pokemon->Speed == other_selected_pokemon->Speed)
     {
-        std::cout << "NOTHING";
+        // half probilitiy
+        int i = std::uniform_int_distribution<>(1, 2)(mersenne);
+        if (i == 1)
+            priority = true;
+        else
+            priority = false;
     }
-    // todo : random
+}
+
+// bag
+void Battle::selectBag()
+{
+    display();
+    int x = 5, y = 16;
+    gotoxy(x, y);
+
+    std::cout << "Quanity    Name            Description" << std::endl;
+    for (int i = 0; i < my_player->items.size(); ++i)
+    {
+        gotoxy(x, y + 1 + i);
+        std::cout << "  X" << my_player->items[i]->item_volume << "     |" <<
+            my_player->items[i]->item_name << "\t|" << my_player->items[i]->item_description <<  std::endl;
+    }
+
+    gotoxy(x, ++y);
+    std::cout << ">\b";
+    while (getArrowkey(x, y) == false)
+    {
+        gotoxy(x, y);
+        std::cout << ">\b";
+    }
+
+    // select
+    if (y == 17)
+        useItme(0);
+    else if (y == 18)
+        useItme(1);
+    else if (y == 19)
+        useItme(2);
+
+    // todo delete when volume = 0
+    // other pokemon will atacky by priority
+
+    while (!getEnterSpacebar()) return;
+
+}
+
+inline void Battle::useItme(int ix)
+{
+    display();
+    gotoxy(5, 15);
+
+    // using monster ball, when battle with other player.
+    if (my_player->items[ix]->item_type == 1 && other_player != nullptr)
+    {
+        std::cout << "Can`t use MonsterBall" << std::endl;
+        while (!getEnterSpacebar()) return;
+
+        return;
+    }
+
+    if (my_player->items[ix]->item_type == 0)
+    {
+        std::cout << "Use " << my_player->items[ix]->item_name << std::endl;
+        // todo..
+        //std::cout << "Recover " << my_selected_pokemon->name << "`s HP about  " <<  my_player->items[ix]->recovery_volume << std::endl;
+        my_player->items[ix]->item_volume -= 1;
+    }
+
+
+
+}
+
+// run
+void Battle::selectRun()
+{
+    system("cls");
+    display();
+    gotoxy(5, 15);
+    std::cout << "NO! There`s no running from a Trainer battle!" << std::endl;
+    while (!getEnterSpacebar());
 }
 
 
@@ -149,12 +587,12 @@ bool Battle::getArrowkey(int& x1, int& y1)
     {
         // UP
     case 72:
-        std::cout << "__";
+        std::cout << " ";
         y1 -= 1;
         break;
         // DOWN
     case 80:
-        std::cout << "_";
+        std::cout << " ";
         y1 += 1;
         break;
         // ENTER
@@ -168,90 +606,17 @@ bool Battle::getArrowkey(int& x1, int& y1)
     return false;
 }
 
-void Battle::selectMove()
+bool Battle::getEnterSpacebar()
 {
-    int x = 5, y = 15;
-    system("cls");
-    display();
-
-    gotoxy(x, y);
-    std::cout << "__MOVES__" << std::endl;
-    std::cout << "     __ " << my_selected_pokemon->move.name << std::endl;;
-    gotoxy(x, y + 1);
-    std::cout << "O\b";
-
-    if (getArrowkey(x, y))
+    int c = 0;
+    switch ((c = getch()))
     {
-        Attack(*my_selected_pokemon, *other_selected_pokemon, my_selected_pokemon->move);
+    case 13:
+        return true;
+    case 32:
+        return true;
+    default:
+        break;
     }
-}
-
-void Battle::Attack(Pokemon& attakingPokemon, Pokemon& defendingPokemon, Move& move)
-{
-    system("cls");
-    display();
-    int x = 5, y = 15;
-    gotoxy(x, y);
-    int damage = calculateDamage(attakingPokemon, defendingPokemon, move);
-    std::cout << "Damage : " << damage << std::endl;
-}
-
-int Battle::calculateDamage(const Pokemon& attakingPokemon, Pokemon& defendingPokemon, const Move& move)
-{
-    float SametypeAttackbonus = 0.0f;
-    if (checkSametypeAttackbonus() == true)
-        SametypeAttackbonus = 1.5f;
-    else
-        SametypeAttackbonus = 1.0f;
-
-    float Decision = 0.0f;
-    // normal Attack
-    if (move.move_type == 0)
-    {
-        Decision = attakingPokemon.Attack * move.power * SametypeAttackbonus;
-    }
-    // sepcial Attack
-    else if (move.move_type == 1)
-    {
-        Decision = attakingPokemon.SpecialAttack * move.power * SametypeAttackbonus;
-    }
-
-    // 0.411 is correction dimension
-    float durability = 0.0f;
-    durability = defendingPokemon.HP * defendingPokemon.Defence * 0.411;
-
-    // Damage calculate
-    calculateType();
-    calculateType();
-    getRanmdomNumber();
-    checkHitSpot();
-
-    float damage = 0.0f;
-    float HitSpot = 1.0f, Type1 = 1.0f, Type2 = 1.0f, RandomNumber = 85.0f;
-    damage = Decision / durability * HitSpot * Type1 * Type2 * RandomNumber;
-
-    return static_cast<int>(damage);
-}
-
-bool Battle::checkSametypeAttackbonus()
-{
-    return true;
-}
-
-float Battle::calculateType()
-{
-    return 0.5f;
-}
-
-bool Battle::checkHitSpot()
-{
-    // 6.25%
-    return true;
-}
-
-int Battle::getRanmdomNumber()
-{
-    float RN = 85.0f;
-    //(217 ~255) * 100 / 255
-    return static_cast<int>(RN);
+    return false;
 }
--- battle/test.h
+++ battle/test.h
@@ -2,6 +2,19 @@
 #include <string>
 #include <vector>
 
+
+class Test
+{
+public:
+    std::string name;
+    Test()
+    {
+        name = "abc";
+    }
+};
+
+
+
 class Move
 {
 public:
@@ -9,6 +22,7 @@ public:
     std::string type;
     int move_type;
     int pp;
+    int max_pp;
     int power;
     int acc;
 
@@ -18,7 +32,9 @@ public:
         name = "fire_blast";
         type = "fire";
         pp = 5;
+        max_pp = 5;
         power = 110;
+    int max_pp;
         acc = 85;
     }
     void hydro_pump()
@@ -28,6 +44,7 @@ public:
         type = "water";
         power = 110;
         pp = 5;
+        max_pp = 5;
         acc = 80;
     }
     
@@ -37,25 +54,27 @@ class Pokemon
 {
 public:
     std::string name;
-	int HP;
+	int maxHP;
 	int Attack;
 	int Defence;
 	int SpecialAttack;
 	int SpecialDefence;
 	int Speed;
 	// int IVs;
-    Move move;
+    Move move[4];
 
 	std::string type1;
 	std::string type2;
 	std::string Nature;
-
+    
+    int HP;
     bool liveState = true;
 
 	void gyarados()
 	{
         name = "gyarados";
-		HP = 95;
+        maxHP = 95;
+        HP = 95;
 		Attack = 125;
 		Defence = 79;
 		SpecialAttack = 60;
@@ -65,13 +84,18 @@ public:
         type1 = "water";
         type2 = "fly";
 
-        move.hydro_pump();
+        move[0].hydro_pump();
+        move[1].hydro_pump();
+        move[2].hydro_pump();
+        move[3].hydro_pump();
 	}
 
     void charizard()
 	{
         name = "charizard";
+        
         HP = 78;
+        maxHP = 78;
 		Attack = 84;
 		Defence = 78;
 		SpecialAttack = 109;
@@ -81,7 +105,10 @@ public:
         type1 = "fire";
         type2 = "fly";
 
-        move.fire_blast();
+        move[0].fire_blast();
+        move[1].fire_blast();
+        move[2].fire_blast();
+        move[3].fire_blast();
 	}
 
     void setNature()
@@ -90,49 +117,108 @@ public:
     }
 };
 
+
 class Player
 {
 public:
+    Pokemon pokemon[6];
     std::vector<Pokemon*> pokemons;
-    int test = 3;
 
-    Player()
-    {
-        
+    Player() {
     }
 
     void charizard()
     {
-        Pokemon pokemon1;
-        pokemon1.charizard();
-        pokemons.push_back(&pokemon1);
+        int i = pokemons.size();
+        pokemon[i].charizard();
+        pokemons.push_back(&pokemon[i]);
+
     }
     void gyarados()
     {
-        Pokemon pokemon1;
-        pokemon1.gyarados();
-        pokemons.push_back(&pokemon1);
+        int i = pokemons.size();
+        pokemon[i].gyarados();
+        pokemons.push_back(&pokemon[i]);
     }
 };
 
-
-
-
-class Test
+class Item
 {
 public:
+    int item_type;
+    int item_volume;
+    std::string item_name;
+    std::string item_description;
+};
 
-    Test(int& i1, int& i2)
+class HPRecoveryItem : public Item
+{
+public:
+    int recovery_volume;
+    
+    void HyperPotion()
     {
-        doSomething(i1, i2);
+        item_type = 0;
+        item_volume = 2;
+        item_name = "HyperPotion";
+        item_description = "Spray-type wound drug. Restores 200 HP of 1 Pokemon.";
+        recovery_volume = 200;
     }
 
-    void doSomething(int& i1, int& i2)
+    void MaxPotion()
     {
-        i1 += 100;
-        i2 += 100;
+        item_type = 0;
+        item_volume = 2;
+        item_name = "MaxPotion";
+        item_description = "Spray-type wound drug. Recover all HP of 1 Pokemon.";
+        recovery_volume = 200;
     }
-
 
 };
+
+class MonsterBallItem : public Item
+{
+public:
+    float chatchProbility;
+
+    void HyperBall()
+    {
+        item_type = 1;
+        item_volume = 2;
+        item_name = "HyperBall";
+        item_description = "A very high-performance ball that makes Pokemon easier to catch than a Super ball";
+        chatchProbility = 99.0f;
+    }
+};
+
+class MyPlayer : public Player
+{
+public:
+    std::vector<Item*> items;
+
+    MonsterBallItem MonsterBallItem;
+    HPRecoveryItem HPRecoveryItem0;
+    HPRecoveryItem HPRecoveryItem1;
+
+    void setItems()
+    {
+        MonsterBallItem.HyperBall();
+        items.push_back(&MonsterBallItem);
+
+        HPRecoveryItem0.HyperPotion();
+        items.push_back(&HPRecoveryItem0);
+
+        HPRecoveryItem1.MaxPotion();
+        items.push_back(&HPRecoveryItem1);
+    }
+};
+
+class OtherPlayer : public Player
+{
+};
+
+
+
+
+
 
