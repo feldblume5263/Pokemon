8c121cb008100f5e305e757619cb7b6577ab730a
dongmin lee <pkalsh345@gmail.com>
2020-12-01 06:00:23 +0900

    데이터베이스 기능 구현 완료: 다른 클래스 모듈로의 요구 메서드 구현 요청 필요

--- DB/Database.cpp
+++ DB/Database.cpp
@@ -27,7 +27,7 @@ void Database::requestPokemon(Pokemon* pokemon, string name) {
 		wcout << "Content-Type : " << r.headers().content_type() << endl;
 		r.extract_json(true).then([&](web::json::value v) {
 			requestTypes(pokemon, v);
-			requestStats(pokemon, v);
+			requestBaseStats(pokemon, v);
 			requestSkills(pokemon, v);
 			}).wait();
 		}).wait();
@@ -35,9 +35,9 @@ void Database::requestPokemon(Pokemon* pokemon, string name) {
 		
 }
 
-// TODO: setType1, setType2 ÎßåÎì§Ïñ¥Ï£ºÏÑ∏Ïöî!
+// TODO: setType1, setType2 ∏∏µÈæÓ¡÷ººø‰!
 void Database::requestTypes(Pokemon* pokemon, json::value& val) {
-	cout << "requestTypes Ìò∏Ï∂ú." << endl;
+	cout << "requestTypes »£√‚." << endl;
 
 	json::value type1, type2;
 	auto types = val.at(L"types");
@@ -82,13 +82,88 @@ void Database::requestTypes(Pokemon* pokemon, json::value& val) {
 		pokemon->setType2(type2_str);
 	}
 
-	cout << "requestTypes ÏôÑÎ£åÎê®." << endl;
+	cout << "requestTypes øœ∑·µ ." << endl;
 }
 
-void Database::requestStats(Pokemon* pokemon, json::value& val) {
+void Database::requestBaseStats(Pokemon* pokemon, json::value& val) {
+	auto stats = val.at(L"stats");
 
+	json::array stats_array = stats.as_array();
+	for (auto it = stats_array.cbegin(); it != stats_array.cend(); it++) {
+		int base_stat = it->at(L"base_stat").as_integer();
+		string_t wstat_name = it->at(L"stat").at(L"name").as_string();
+		string stat_name;
+
+		stat_name.assign(wstat_name.begin(), wstat_name.end());
+		pokemon->setBaseStat(stat_name, base_stat);
+	}
 }
 
-void Database::requestStats(Pokemon* pokemon, json::value& val) {
+// Pokemonø°º≠ Ω∫≈≥ ∞≥ºˆ∏¶ ∞Æ∞Ì ¿÷¿∏∏È ¡¡¿Ω
+// Skill¿« πËø≠¿∫ ∆˜¿Œ≈Õ∑Œ ∞Æ∞Ì ¿÷æÓæﬂ«‘
+void Database::requestSkills(Pokemon* pokemon, json::value& val) {
+	auto moves = val.at(L"moves");
 
+	json::array moves_array = moves.as_array();
+	size_t num_of_moves = moves_array.size();
+	vector<bool> is_used_move;
+
+	uniform_int_distribution<int> dis(0, num_of_moves - 1);
+	size_t num_of_available_moves = num_of_moves;
+	size_t move_setting_idx = 0;
+	is_used_move.assign(num_of_moves, false);
+
+	while (num_of_available_moves != 0) {
+		int move_idx = dis(gen);
+		if (!is_used_move[move_idx]) {
+			string_t url = moves.at(L"move").at(L"url").as_string();
+			is_used_move[move_idx] = true;
+			num_of_available_moves -= 1;
+			
+			requestSkillURL(pokemon, url, move_setting_idx);
+			if (move_setting_idx == 4) return;
+		}
+	}
+}
+
+bool requestSkillURL(Pokemon* pokemon, string_t& url, size_t& move_setting_idx) {
+	uri_builder builder(url);
+
+	uri resource(builder.to_uri());
+	http_client client(resource);
+
+	http_request req(methods::GET);
+
+	client.request(req).then([pokemon, &move_setting_idx](http_response r) {
+		
+		r.extract_json(true).then([pokemon, &move_setting_idx](web::json::value v) {
+			if (v.at(L"power").is_null()) {
+				return;
+			}
+			else {
+				Skill* added_skill = new Skill();
+				string_t wname = v.at(L"name").as_string();
+				string_t wdamage_type = v.at(L"damage_class").at(L"name").as_string();
+				string_t wtype = v.at(L"type").at(L"name").as_string();
+				int power = v.at(L"power").as_integer();
+				int accuracy = v.at(L"accuracy").as_integer();
+				int pp = v.at(L"pp").as_integer();
+
+				string name, damage_type, type;
+				name.assign(wname.begin(), wname.end());
+				damage_type.assign(wdamage_type.begin(), wdamage_type.end());
+				type.assign(wtype.begin(), wtype.end());
+
+				added_skill->setName(name);
+				added_skill->setDamageType(damage_type);
+				added_skill->setType(type);
+				added_skill->setPower(power);
+				added_skill->setAccuracy(accuracy);
+				added_skill->setPP(pp);
+
+				pokemon->setSkill(move_setting_idx, added_skill);
+			}
+
+			}).wait();
+		}).wait();
 }
--- DB/Database.h
+++ DB/Database.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
+#include "include/skill.h"
 #include "include/pokemon.h"
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <iomanip>
 
 using namespace utility;
 using namespace web;
@@ -19,13 +21,21 @@ public:
 	~Database();
 	void requestPokemon(Pokemon*, string);
 	void requestTypes(Pokemon*, json::value&);
-	void requestStats(Pokemon*, json::value&);
+	void requestBaseStats(Pokemon*, json::value&);
 	void requestSkills(Pokemon*, json::value&);
+	void requestSkillURL(Pokemon*, string_t&, size_t&);
 
 private:
 	const string URI = "https://pokeapi.co/api/v2/";
 	random_device *rd;
 	mt19937 *gen;
-	
 };
 
+/*
+	this->name = "Thunderbolt";
+	this->power = 90;
+	this->accuracy = 100;
+	this->type = Electric;
+	this->damage_type = Special;
+	this->pp = 20;
+*/
